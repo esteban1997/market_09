aed10440043059f7670214b3003ea5cf6754b7ed
Esteban Aguirre <estebanaguirre1997@gmail.com>
2022-04-22 16:57:19 -0500

    Se agrega login, registrarse y peticiones http

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: prefer_const_constructors, non_constant_identifier_names, use_key_in_widget_constructors, avoid_print, constant_identifier_names
 
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:market_09/pages/product/products_page.dart';
 import 'package:market_09/pages/register_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   static const String ROUTE = "/login";
@@ -17,11 +22,15 @@ class _LoginPageState extends State<LoginPage> {
 
   final _passwordController = TextEditingController();
 
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   bool _obscurePawword = true;
+  bool _isSubmitedText = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(title: Text("Login")),
       body: SingleChildScrollView(
         child: Form(
@@ -38,70 +47,149 @@ class _LoginPageState extends State<LoginPage> {
                   SizedBox(
                     height: 5,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: TextFormField(
-                      controller: _emailController,
-                      validator: (value) =>
-                          !value!.contains('@') ? 'Email invalido' : null,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Email",
-                          hintText: "Coloque email",
-                          icon: Icon(Icons.email)),
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: _obscurePawword,
-                    controller: _passwordController,
-                    validator: (value) =>
-                        value!.length < 5 ? 'Contraseña invalido' : null,
-                    decoration: InputDecoration(
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            _obscurePawword = !_obscurePawword;
-                            setState(() {});
-                          },
-                          child: Icon(_obscurePawword
-                              ? Icons.visibility
-                              : Icons.visibility_off),
-                        ),
-                        border: OutlineInputBorder(),
-                        labelText: "Password",
-                        hintText: "Ingrese password",
-                        icon: Icon(Icons.lock)),
-                  ),
-                  Column(
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: Theme.of(context).primaryColor),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              print("Formulario valido");
-                            } else {
-                              print("errores");
-                            }
-                          },
-                          child: Text(
-                            "Login",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .copyWith(color: Colors.white),
-                          )),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(
-                                context, RegisterPage.ROUTE);
-                          },
-                          child: Text("Registrarse"))
-                    ],
-                  )
+                  _emailTF(),
+                  _passwordTF(),
+                  _actionsTF(context)
                 ],
               )),
         ),
       ),
     );
+  }
+
+  Column _actionsTF(BuildContext context) {
+    return Column(
+      children: [
+        _isSubmitedText
+            ? CircularProgressIndicator()
+            : ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).primaryColor),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    print("Formulario valido");
+                    _loginUser();
+                    _redirectUser();
+                  } else {
+                    print("errores");
+                  }
+                },
+                child: Text(
+                  "Login",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(color: Colors.white),
+                )),
+        TextButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, RegisterPage.ROUTE);
+            },
+            child: Text("Registrarse"))
+      ],
+    );
+  }
+
+  Padding _emailTF() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8),
+      child: TextFormField(
+        controller: _emailController,
+        validator: (value) => !value!.contains('@') ? 'Email invalido' : null,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Email",
+            hintText: "Coloque email",
+            icon: Icon(Icons.email)),
+      ),
+    );
+  }
+
+  TextFormField _passwordTF() {
+    return TextFormField(
+      obscureText: _obscurePawword,
+      controller: _passwordController,
+      validator: (value) => value!.length < 5 ? 'Contraseña invalido' : null,
+      decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            onTap: () {
+              _obscurePawword = !_obscurePawword;
+              setState(() {});
+            },
+            child:
+                Icon(_obscurePawword ? Icons.visibility : Icons.visibility_off),
+          ),
+          border: OutlineInputBorder(),
+          labelText: "Password",
+          hintText: "Ingrese password",
+          icon: Icon(Icons.lock)),
+    );
+  }
+
+  void _loginUser() async {
+    _isSubmitedText = true;
+    setState(() {});
+    print(_emailController.text);
+    print(_passwordController.text);
+    final res = await http
+        .post(Uri.parse('http://10.0.2.2:1337/api/auth/local'), body: {
+      "identifier": _emailController.text,
+      "password": _passwordController.text,
+    });
+
+    _isSubmitedText = false;
+    setState(() {});
+
+    final responseData = json.decode(res.body);
+
+    if (res.statusCode == 200) {
+      print("correcto");
+      print(responseData);
+      _succesResponse();
+      _storeUserData(responseData);
+    } else {
+      print("incorrecto");
+      print(responseData);
+      _errorResponse(responseData['error']['message']);
+    }
+
+    print(responseData);
+  }
+
+  void _succesResponse() {
+    final _snackBar = SnackBar(
+        backgroundColor: Colors.green,
+        content: Text(
+          "Usuario ${_emailController.text} correcto",
+          style: TextStyle(color: Colors.white),
+        ));
+
+    ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+  }
+
+  void _errorResponse(var messageError) {
+    final _snackBar = SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(
+          messageError,
+          style: TextStyle(color: Colors.white),
+        ));
+
+    ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+  }
+
+  void _storeUserData(Map<String, dynamic> responseData) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString("jwt", responseData['jwt']);
+    prefs.setInt("id", responseData['user']['id']);
+    prefs.setString("username", responseData['user']['username']);
+    prefs.setString("email", responseData['user']['email']);
+  }
+
+  void _redirectUser() {
+    Future.delayed(Duration(milliseconds: 2000), () {
+      Navigator.pushReplacementNamed(context, ProductsPage.ROUTE);
+    });
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: prefer_const_constructors, non_constant_identifier_names, use_key_in_widget_constructors, avoid_print, constant_identifier_names
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:market_09/pages/login_page.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:market_09/pages/product/products_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterPage extends StatefulWidget {
   static const String ROUTE = "/register";
@@ -12,6 +18,7 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   final _userController = TextEditingController();
   final _emailController = TextEditingController();
@@ -21,10 +28,12 @@ class _RegisterPageState extends State<RegisterPage> {
 
   bool _obscurePawword = true;
   bool _obscureConfirmPawword = true;
+  bool _isSubmitedText = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(title: Text("Registrarse")),
       body: SingleChildScrollView(
         child: Form(
@@ -41,127 +50,218 @@ class _RegisterPageState extends State<RegisterPage> {
                   SizedBox(
                     height: 5,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: TextFormField(
-                      controller: _emailController,
-                      validator: (value) {
-                        if (!value!.contains('@')) {
-                          return 'Email invalido';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Email",
-                          hintText: "Coloque email",
-                          icon: Icon(Icons.email)),
-                    ),
-                  ),
+                  _emailTF(),
                   SizedBox(
                     height: 5,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: TextFormField(
-                      controller: _userController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Ingrese un usuario";
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Usuario",
-                          hintText: "Ingrese su usuario",
-                          icon: Icon(Icons.account_circle)),
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: _obscurePawword,
-                    controller: _passwordController,
-                    validator: (value) =>
-                        value!.length < 5 ? 'Contraseña invalido' : null,
-                    decoration: InputDecoration(
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            _obscurePawword = !_obscurePawword;
-                            setState(() {});
-                          },
-                          child: Icon(_obscurePawword
-                              ? Icons.visibility
-                              : Icons.visibility_off),
-                        ),
-                        border: OutlineInputBorder(),
-                        labelText: "Password",
-                        hintText: "Ingrese password",
-                        icon: Icon(Icons.lock)),
-                  ),
+                  _usernameTF(),
                   SizedBox(
                     height: 5,
                   ),
-                  TextFormField(
-                    obscureText: _obscureConfirmPawword,
-                    controller: _confirmPasswordController,
-                    validator: (value) {
-                      if (_passwordController.text != value) {
-                        return 'Las contraseñas no coinciden';
-                      }
-                      if (value!.isEmpty) {
-                        return 'Confirme la contraseña';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            _obscureConfirmPawword = !_obscureConfirmPawword;
-                            setState(() {});
-                          },
-                          child: Icon(_obscureConfirmPawword
-                              ? Icons.visibility
-                              : Icons.visibility_off),
-                        ),
-                        border: OutlineInputBorder(),
-                        labelText: "Confirmar Password",
-                        hintText: "Repita el password",
-                        icon: Icon(Icons.lock)),
+                  _passwordTF(),
+                  SizedBox(
+                    height: 15,
                   ),
+                  _confirmPasswordTF(),
                   SizedBox(
                     height: 5,
                   ),
-                  Column(
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: Theme.of(context).primaryColor),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              print("Formulario valido");
-                            } else {
-                              print("errores");
-                            }
-                          },
-                          child: Text(
-                            "Registrarse",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .copyWith(color: Colors.white),
-                          )),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(
-                                context, LoginPage.ROUTE);
-                          },
-                          child: Text("ir al login"))
-                    ],
-                  )
+                  _actionsTF(context)
                 ],
               )),
         ),
       ),
     );
+  }
+
+  Widget _emailTF() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8),
+      child: TextFormField(
+        controller: _emailController,
+        validator: (value) {
+          if (!value!.contains('@')) {
+            return 'Email invalido';
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Email",
+            hintText: "Coloque email",
+            icon: Icon(Icons.email)),
+      ),
+    );
+  }
+
+  Widget _usernameTF() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8),
+      child: TextFormField(
+        controller: _userController,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return "Ingrese un usuario";
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Usuario",
+            hintText: "Ingrese su usuario",
+            icon: Icon(Icons.account_circle)),
+      ),
+    );
+  }
+
+  Widget _confirmPasswordTF() {
+    return TextFormField(
+      obscureText: _obscureConfirmPawword,
+      controller: _confirmPasswordController,
+      validator: (value) {
+        if (_passwordController.text != value) {
+          return 'Las contraseñas no coinciden';
+        }
+        if (value!.isEmpty) {
+          return 'Confirme la contraseña';
+        }
+
+        return null;
+      },
+      decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            onTap: () {
+              _obscureConfirmPawword = !_obscureConfirmPawword;
+              setState(() {});
+            },
+            child: Icon(_obscureConfirmPawword
+                ? Icons.visibility
+                : Icons.visibility_off),
+          ),
+          border: OutlineInputBorder(),
+          labelText: "Confirmar Password",
+          hintText: "Repita el password",
+          icon: Icon(Icons.lock)),
+    );
+  }
+
+  Widget _passwordTF() {
+    return TextFormField(
+      obscureText: _obscurePawword,
+      controller: _passwordController,
+      validator: (value) => value!.length < 7 ? 'Contraseña invalido' : null,
+      decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            onTap: () {
+              _obscurePawword = !_obscurePawword;
+              setState(() {});
+            },
+            child:
+                Icon(_obscurePawword ? Icons.visibility : Icons.visibility_off),
+          ),
+          border: OutlineInputBorder(),
+          labelText: "Password",
+          hintText: "Ingrese password",
+          icon: Icon(Icons.lock)),
+    );
+  }
+
+  Widget _actionsTF(BuildContext context) {
+    return Builder(
+      builder: (context) => Column(
+        children: [
+          _isSubmitedText
+              ? CircularProgressIndicator()
+              : ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("Formulario valido");
+                      _registerUser();
+                    } else {
+                      print("errores");
+                    }
+                  },
+                  child: Text(
+                    "Registrarse",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.white),
+                  )),
+          TextButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, LoginPage.ROUTE);
+              },
+              child: Text("ir al login"))
+        ],
+      ),
+    );
+  }
+
+  void _registerUser() async {
+    _isSubmitedText = true;
+    setState(() {});
+
+    final res = await http
+        .post(Uri.parse('http://10.0.2.2:1337/api/auth/local/register'), body: {
+      "username": _userController.text,
+      "email": _emailController.text,
+      "password": _passwordController.text,
+    });
+
+    final responseData = json.decode(res.body);
+    _isSubmitedText = false;
+
+    setState(() {});
+    if (res.statusCode == 200) {
+      print("respuesta 200");
+      print(res.body);
+      _succesResponse();
+      _storeUserData(responseData);
+      _redirectUser();
+    } else {
+      print("se presento algun error");
+      print(res.body);
+      _errorResponse(responseData['error']['message']);
+    }
+  }
+
+  void _succesResponse() {
+    final _snackBar = SnackBar(
+        backgroundColor: Colors.green,
+        content: Text(
+          "Usuario ${_userController.text} creado \\°n°\\ /°n°/ \\°n°\\ /°n°/",
+          style: TextStyle(color: Colors.white),
+        ));
+
+    ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+  }
+
+  void _errorResponse(var messageError) {
+    final _snackBar = SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(
+          messageError,
+          style: TextStyle(color: Colors.white),
+        ));
+
+    ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+  }
+
+  void _storeUserData(Map<String, dynamic> responseData) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString("jwt", responseData['jwt']);
+    prefs.setInt("id", responseData['user']['id']);
+    prefs.setString("username", responseData['user']['username']);
+    prefs.setString("email", responseData['user']['email']);
+  }
+
+  void _redirectUser() {
+    Future.delayed(Duration(milliseconds: 2000), () {
+      Navigator.pushReplacementNamed(context, ProductsPage.ROUTE);
+    });
   }
 }
